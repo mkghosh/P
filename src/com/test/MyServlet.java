b9fe4b4044912c081eec5febdebb9ce198fa945d
Mithun Kumer Ghose <mkumerg@gmail.com>
2017-03-30 07:48:48 +0600

    Final Commit to P

--- src/com/test/MyServlet.java
+++ src/com/test/MyServlet.java
@@ -2,6 +2,7 @@ package com.test;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 
 import javax.servlet.Servlet;
 import javax.servlet.ServletConfig;
@@ -10,6 +11,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import com.test.exp.MarksNotDefined;
 
@@ -19,6 +21,7 @@ import com.test.exp.MarksNotDefined;
 @WebServlet("/MyServlet")
 public class MyServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private static final String ST_ID_LIST = "stIdList";
        
     /**
      * @see HttpServlet#HttpServlet()
@@ -46,18 +49,35 @@ public class MyServlet extends HttpServlet {
 		Student student = getStudent(request);
 		student.setMarks(marks);
 		Result result = null;
+		try {
+			result = new Result(student.getStId(), student.getName(), student.getCumulativeGrade());
+		} catch (MarksNotDefined mnd) {
+			System.out.println(mnd.getMessage());
+		}
 		String buttonType = request.getParameter("action");
+		ArrayList<String> stIdList = null;
 		if(buttonType.equals("Next")) {
+			HttpSession session = request.getSession(true);
+			stIdList = (ArrayList<String>) session.getAttribute(ST_ID_LIST);
+			if(stIdList == null) 
+				stIdList = new ArrayList<>();
+			stIdList.add(result.getStID());
+			session.setAttribute(ST_ID_LIST, stIdList);
+			session.setAttribute(result.getStID(), result);
 			response.sendRedirect("/P/html/submit_test.html");
 		} else {
-
-			try {
-				result = new Result(student.getStId(), student.getName(), student.getCumulativeGrade());
-			} catch (MarksNotDefined mnd) {
-				System.out.println(mnd.getMessage());
+			HttpSession session = request.getSession();
+			ArrayList<String> idList = (ArrayList<String>)session.getAttribute(ST_ID_LIST);
+			for(int i = 0; i < idList.size(); i++) {
+				Result stResult = (Result)session.getAttribute(idList.get(i));
+				
+				out.println("<p>Mr./Miss : " + stResult.getStName() + "<br/>"
+						+ "Your id is : " + stResult.getStID() + "<br/>"
+						+ "You have obtained " + stResult.getCgpa() + "<br/>" +
+						"Your grade is : " + stResult.getGrade());
 			}
-			
 			out.println("<p>Mr./Miss : " + result.getStName() + "<br/>"
+					+ "Your Id is : " + result.getStID() + "<br/>" 
 					+ "You have obtained " + result.getCgpa() + "<br/>" +
 					"Your grade is : " + result.getGrade());
 		}	
@@ -68,7 +88,7 @@ public class MyServlet extends HttpServlet {
 		String lastName = request.getParameter("last_name");
 		String dept = request.getParameter("dept");
 		String studentId = request.getParameter("st_id");
-		return new Student(firstName + " " + lastName, dept, studentId);
+		return new Student(firstName + " " + lastName, studentId, dept);
 	}
 
 	private Marks getMarks(HttpServletRequest request) {
